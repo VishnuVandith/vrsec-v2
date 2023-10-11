b43dbce52766e68778141ee30d0990d27b6e1f85
Vishnu Vandith G <39836113+VishnuVandith@users.noreply.github.com>
2023-10-11 16:06:17 +0530

    Add files via upload
    Commit for Portfolio

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,31 @@
+import "./topbar.scss";
+import { Person, Mail } from "@material-ui/icons";
+
+export default function Topbar({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            Vishnu Vandith G.
+          </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>+123 456 789</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>vishnuvandith@gmail.com</span>
+          </div>
+        </div>
+        <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
